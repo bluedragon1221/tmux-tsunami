d3d7d0e5abf55b3fb1db4bc928fcccc255c392c6
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2025-10-05 08:17:15 -0500

    make colors configurable

--- modules/common/bar.nix
+++ modules/common/bar.nix
@@ -1,52 +1,67 @@
-{tsunamiLib, ...}: let
+{
+  config,
+  lib,
+  tsunamiLib,
+  ...
+}: let
   inherit (tsunamiLib) scriptPath;
 in {
-  tsunami = {
-    scripts = {
-      "battery" = ''
-        energy_now=$(cat /sys/class/power_supply/BAT0/energy_now)
-        energy_full=$(cat /sys/class/power_supply/BAT0/energy_full)
-        percentage=$((energy_now * 100 / energy_full))
-        printf "%.0f%%" "$percentage"
-      '';
+  options = let
+    hexColor = lib.types.strMatching "^#?([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$";
+  in {
+    tsunami.theme = {
+      bg = lib.mkOption {
+        type = hexColor;
+      };
+      bg_dark = lib.mkOption {
+        type = hexColor;
+      };
+    };
+  };
+  config = {
+    tsunami = {
+      scripts = {
+        "battery" = ''
+          energy_now=$(cat /sys/class/power_supply/BAT0/energy_now)
+          energy_full=$(cat /sys/class/power_supply/BAT0/energy_full)
+          percentage=$((energy_now * 100 / energy_full))
+          printf "%.0f%%" "$percentage"
+        '';
 
-      "bar" = ''
-        bg="#1e1e2e"
+        "bar" = ''
+          tmux set -g status on
 
-        tmux set -g status on
+          tmux set -g status-position bottom
+          tmux set -g status-justify absolute-centre
+          tmux set -g status-bg "${config.tsunami.theme.bg}"
 
-        tmux set -g status-position bottom
-        tmux set -g status-justify absolute-centre
-        tmux set -g status-bg "$bg"
+          tmux set -g status-left-style fg=green,bold
+          tmux set -g status-left " #{client_session}"
 
-        tmux set -g status-left-style fg=green,bold
-        tmux set -g status-left " #{client_session}"
+          tmux set -g window-status-style fg=color243
+          tmux set -g window-status-format " #I "
+          tmux set -g window-status-current-style fg=color12,bold
+          tmux set -g window-status-current-format " #I "
+          tmux set -g window-status-separator ""
 
-        tmux set -g window-status-style fg=color243
-        tmux set -g window-status-format " #I "
-        tmux set -g window-status-current-style fg=color12,bold
-        tmux set -g window-status-current-format " #I "
-        tmux set -g window-status-separator ""
+          tmux set -g status-right "#[fg=color243]%l:%M  #[fg=red]#(${scriptPath "battery"}) "
 
-        tmux set -g status-right "#[fg=color243]%l:%M  #[fg=red]#(${scriptPath "battery"}) "
+          ## pane borders
+          tmux set -g pane-border-style fg=${config.tsunami.theme.bg_dark},bg=${config.tsunami.theme.bg_dark}
+          tmux set -g pane-active-border-style fg=${config.tsunami.theme.bg_dark},bg=${config.tsunami.theme.bg_dark}
 
-        bg_dark="#181825"
+          ## pane backgrounds
+          # Set the foreground/background color for the active window
+          tmux set -g window-active-style bg=${config.tsunami.theme.bg}
 
-        ## pane borders
-        tmux set -g pane-border-style fg=$bg_dark,bg=$bg_dark
-        tmux set -g pane-active-border-style fg=$bg_dark,bg=$bg_dark
+          # Set the foreground/background color for all other windows
+          tmux set -g window-style bg=${config.tsunami.theme.bg_dark}
+        '';
+      };
 
-        ## pane backgrounds
-        # Set the foreground/background color for the active window
-        tmux set -g window-active-style bg=$bg
-
-        # Set the foreground/background color for all other windows
-        tmux set -g window-style bg=$bg_dark
+      confs."bar" = ''
+        run-shell ${scriptPath "bar"}
       '';
     };
-
-    confs."bar" = ''
-      run-shell ${scriptPath "bar"}
-    '';
   };
 }
